089c32d79a2399a9eccd36c9290dee7b9fb6f86f
Ryusei Sakamoto <tmc1487518sr@gmail.com>
2023-06-28 03:05:39 +0900

    render

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Swipe from "./components/Swipe";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import { SignUp2 } from "./components/SignUp2";
-const URL = "http://localhost:8000";
+const URL = "https://syamot.onrender.com";
 // process.env.NODE_ENV === "production"
 //   ? "https://syamot.onrender.com"
 //   : "http://localhost:8000";
