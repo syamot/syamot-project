32b8e002e608aa5b9bba9a2c85398942fdd5fb2d
Ryusei Sakamoto <tmc1487518sr@gmail.com>
2023-07-05 20:40:25 +0900

    バグ修正

--- db/knexfile.js
+++ db/knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      user: process.env.DB_USER || "saitorui",
+      user: process.env.DB_USER || "RYUSEI",
       database: process.env.DB_NAME || "syamot",
       password: process.env.DB_PASSWORD,
     },
--- src/App.js
+++ src/App.js
@@ -46,8 +46,8 @@ function App() {
   const [beforeFlag, setBeforeFlag] = useState("");
   const [editItem, setEditItem] = useState({});
   useEffect(() => {
-    console.log("selectImg===========", selectImg)
-  }, [selectImg])
+    console.log("selectImg===========", selectImg);
+  }, [selectImg]);
   const [tradingHistory, setTradingHistory] = useState("");
 
   const [flagHistory, setFlagHistory] = useState(["list"]);
@@ -103,9 +103,11 @@ function App() {
       setExhibitList(userItemData);
 
       // 期限切れデータリスト作成
-      const AddDeadLineList = userItemData.filter((elem) => new Date(elem.item_deadline) < new Date())
-      setDeadLineList(AddDeadLineList)
-      console.log("AddDeadLineList============", AddDeadLineList)
+      const AddDeadLineList = userItemData.filter(
+        (elem) => new Date(elem.item_deadline) < new Date()
+      );
+      setDeadLineList(AddDeadLineList);
+      console.log("AddDeadLineList============", AddDeadLineList);
 
       const userPurchaseList = itemData.filter(
         (elem) => elem.buyer_id === openUserId
@@ -218,7 +220,6 @@ function App() {
             userData={userData}
             URL={URL}
             setSelectImg={setSelectImg}
-            // oneUser={oneUser}
             setOneUser={setOneUser}
           />
           <Footer setSelectFlag={setSelectFlag} />
@@ -226,7 +227,6 @@ function App() {
       );
     //
     case "contactList":
-      console.log("contactList");
       return (
         <>
           <Header
