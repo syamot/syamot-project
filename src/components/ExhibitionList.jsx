64557f8714f08be0d99a3793f7c2802faec95413
Ryusei Sakamoto <tmc1487518sr@gmail.com>
2023-07-06 13:45:52 +0900

    出品リスト作成

--- src/components/ExhibitionList.jsx
+++ src/components/ExhibitionList.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./style/exhibitionList.css";
-import { GrNext } from "react-icons/gr";
 import { AiFillWarning } from "react-icons/ai";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
@@ -32,50 +31,31 @@ const ExhibitionList = (props) => {
     setSelectFlag("card");
   };
 
-  const editItem = (e) => {
-    // console.log("editItem入れるよ====", exhibitList[0].id);
-    console.log("tagName=====", e.target.tagName);
-    if (e.target.tagName === "path") return;
-    let item = exhibitList.filter((elem) => elem.id === Number(e.target.id));
-    console.log("editItem入れるよ====", item);
-    setEditItem(item[0]);
-    console.log(selectFlag);
-    setBeforeFlag(selectFlag);
-    setSelectFlag("post");
-  };
-
-  // useEffect(()=>{
-  //   if(deadLineList.length!==0){
-
-  //   }
-  // },[])
-
-  // className={
-  //   deadLineList.some((obj) => obj.id === item.id)
-  //     ? "exhibition-image-item active"
-  //     : "exhibition-image-item"
-  // }
   return (
     <div className="exhibition-list-box">
       <div className="exhibition-piece">
-        <IoIosArrowBack className="exhibition-navi-icon" />
+        <IoIosArrowBack
+          className="exhibition-navi-icon"
+          onClick={() => setSelectFlag("myPage")}
+        />
         <h2 className="exhibition-title">出品リスト</h2>
-        <div></div>
-        <AiFillWarning className="exhibition-warningIcon" />
+        <div className="exhibition-position-adjustment"></div>
       </div>
       <ul className="exhibition-image-list">
         {exhibitList.length !== 0 &&
           exhibitList.map((item) => (
-            <li key={item.id} className={"exhibition-image-item"}>
+            <li key={item.id} className="exhibition-image-item">
               <div className="exhibition-image-box">
                 <div className="exhibition-imgBlock">
                   <img src={item.item_img[0]} alt={item.item_name}></img>
                   <div className="exhibition-info">
-                    {/* {deadLineList.some((obj) => obj.id === item.id) && (
-                      <div className="warningBlock"></div>
-                    )} */}
                     <p>商品名:{item.item_name}</p>
-                    <p>期限:{item.item_deadline.split("T")[0]}</p>
+                    <div className="exhibition-warning">
+                      <p>期限:{item.item_deadline.split("T")[0]}</p>
+                      {deadLineList.some((obj) => obj.id === item.id) && (
+                        <AiFillWarning className="exhibition-warningIcon" />
+                      )}
+                    </div>
                     <p>商品の状態:{item.item_status}</p>
                   </div>
                 </div>
